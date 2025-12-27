287e53c2ac64d54ba3e8c2df671ffcd2ecfff083
JoaoAbreu-sccp <jvsabru@gmail.com>
2025-12-27 15:26:34 -0300

    def registar disco

--- main.py
+++ main.py
@@ -5,13 +5,34 @@ ARQ = "discos.json"
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def load():
+def Load():
     if not os.path.exists(ARQ):
         with open(ARQ, "w", encoding="utf-8") as f:
             json.dump([], f, ensure_ascii=False, indent=4)
+
     with open(ARQ, "r", encoding="utf-8") as f:
         return json.load(f)
 
-def save(dados):
+def Save(dados):
     with open(ARQ, "w", encoding="utf-8") as f:
         json.dump(dados, f, ensure_ascii=False, indent=4)
+
+def DiscRegister():
+    dados = Load()
+
+    nome = str(input("Nome do disco: "))
+    autor = str(input("Artista/Banda: "))
+    ano = int(input("Ano de lançamento: "))
+    print("DIgite abaixo a sua critica:")
+    critica = str(input("-> "))
+
+    disco = {
+        'nome': nome,
+        'autor': autor,
+        'ano': ano,
+        'critica': critica
+    }
+    dados.append(disco)
+    Save(dados)
+    print("Disco catalogado com sucesso.")
+
